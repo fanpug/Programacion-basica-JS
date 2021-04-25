50a58519fe31fe67ea8d5485ab5aa12d4186185e
Humberto Navarro <a320669@uach.mx>
2021-04-24 23:51:28 -0600

    Se agrega el programa 5, imprimir numeros impares hasta el 100 y cuantos son

---
+++ 6.js
@@ -0,0 +1,13 @@
+//Programa 6. Imprimir todos los numeros desde 1 hasta el numero que de el usuario
+let numImpares = 0;
+
+for(let i = 0; i <= 100; i++){
+    if(i % 2 == 0){
+
+    }else{
+      console.log(i);
+      numImpares++;
+    }
+}
+
+console.log("Existen " + numImpares + " numeros impares.");

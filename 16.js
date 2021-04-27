86fee8ac4739ed244811fbd8ff172086f37cc677
Humberto Navarro <a320669@uach.mx>
2021-04-27 17:04:35 -0600

    16. Imprimir diez veces la serie de números del 1 al 10.

---
+++ 16.js
@@ -0,0 +1,9 @@
+//Programa 16. Imprimir diez veces la serie de números del 1 al 10.
+function print() {
+  for (let i = 0; i < 10; i++) {
+    for (let j = 1; j <= 10; j++)
+      console.log(j);
+  }
+}
+
+print();
